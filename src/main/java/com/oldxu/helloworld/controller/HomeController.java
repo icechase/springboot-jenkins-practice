dff3297d02de3f5922351b32ba83f3048cb6bfc4
Oldxu <oldxuedu@qq.com>
2022-10-17 18:58:24 +0800

    jenkins v1

--- src/main/java/com/oldxu/helloworld/controller/HomeController.java
+++ src/main/java/com/oldxu/helloworld/controller/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 
     @RequestMapping("/")
     String home() {
-        return "Hello from Oldxu Kubernetes on Jenkins v1";
+        return "Hello from Oldxu Kubernetes on Jenkins v2";
     }
 
 }
